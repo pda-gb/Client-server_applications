aa5b3e68dfb5a5aaae68f6538a6b4cc7f9bc4996
Denis <pda.prostor@gmail.com>
2020-11-25 20:37:29 +0300

    fix error E999

--- less_1/task_3.py
+++ less_1/task_3.py
@@ -1,11 +1,12 @@
-w1 = b'attribute'
-w2 = b'класс'
-w3 = b'функция'
-w4 = b'type'
+w1 = 'attribute'
+w2 = 'класс'
+w3 = 'функция'
+w4 = 'type'
 words = [w1, w2, w3, w4]
 
 for itm in words:
     try:
-        print(f'[{itm}] is {type(itm)}')
-    except SyntaxError:
-        print("невозможно записать в байтовом типе с помощью маркировки b''")
+        print(f"[{itm}] is bytes {bytes(itm, 'ascii') }")
+    except UnicodeEncodeError:
+        print(f"[{itm}] невозможно записать в байтовом типе с помощью"
+              f" маркировки b''")

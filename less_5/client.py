57c23c242b6f26ac24259e768e177f152475ebdc
Denis <pda.prostor@gmail.com>
2020-12-09 23:30:21 +0300

    less_5: fix

--- less_5/client.py
+++ less_5/client.py
@@ -3,8 +3,8 @@ import socket
 import sys
 import time
 
-from less_4.common.utils import send_message, get_message
-from less_4.common.variables import DEFAULT_IP_ADDRESS, DEFAULT_PORT, ACTION, \
+from less_5.common.utils import send_message, get_message
+from less_5.common.variables import DEFAULT_IP_ADDRESS, DEFAULT_PORT, ACTION, \
     PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR
 
 
